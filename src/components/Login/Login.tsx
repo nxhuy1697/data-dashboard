e42f4d8645ac237382640467b7bd54fd7e6df8c1
nxhuy1697 <nxhuy1697@gmail.com>
2024-04-10 17:35:16 +0700

    first commit

---
+++ src/components/Login/Login.tsx
@@ -0,0 +1,24 @@
+
+import { Button } from "@mui/material";
+import { useSession, signIn, signOut } from "next-auth/react"
+
+
+const Login = () => {
+    const {data : session } = useSession();
+
+
+    if (session) {
+        return <>
+        <h1>
+        Signed in as {session?.user?.email}
+        </h1>      
+        <Button variant="contained" color="error" onClick={() => signOut()} > Sign out</Button>
+        </>
+    }
+    return<>
+       <h2>Please Login</h2>
+        <Button variant="contained" color="success" onClick={() => signIn()}> Sign in</Button>
+    </>
+}
+export default Login;
+
